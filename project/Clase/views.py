ae49b38b58b16e31abd00ada08cc4b2a77fa4bb1
Jean Villegas <jeanvillegaslop@gmail.com>
2024-04-29 15:31:39 -0500

    add: htmls and querys

--- project/Clase/views.py
+++ project/Clase/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 from . import models
 
 def home(request):
-    ...
+    query = models.Ciclo.objects.all().prefetch_related('cursos__profesor', 'cursos__temas__ejercicios__solucion')
+    context = {'ciclos': query}
+    return render(request, 'Clase/index.html', context)
--- project/core/urls.py
+++ project/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-app_name = 'Clase'
+app_name = 'core'
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('', views.home, name="home"),
 ]
