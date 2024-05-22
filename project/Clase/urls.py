626acda3c7e39fbcca78f1256de151f6bf876635
Jean Villegas <jeanvillegaslop@gmail.com>
2024-05-22 18:25:57 -0500

    add crud ejercicio

--- project/Clase/urls.py
+++ project/Clase/urls.py
@@ -4,7 +4,13 @@ app_name = 'Clase'
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("ejerciciocategoria/create/", views.ejerciciocategoria_create, name="ejerciciocategoria_create"),
+    #path("ejerciciocategoria/create/", views.ejerciciocategoria_create, name="ejerciciocategoria_create"),
+    path("ejerciciocategoria/create/", views.EjercicioCategoriaCreate.as_view(), name="ejerciciocategoria_create"),
+    #path("ejerciciocategoria/list/", views.ejerciciocategoria_list, name="ejerciciocategoria_list"),
+    path("ejerciciocategoria/list/", views.EjercicioCategoriaList.as_view(), name="ejerciciocategoria_list"),
+    path("ejerciciocategoria/detail/<int:pk>/", views.EjercicioCategoriaDetail.as_view(), name="ejerciciocategoria_detail"),
+    path("ejerciciocategoria/update/<int:pk>/", views.EjercicioCategoriaUpdate.as_view(), name="ejerciciocategoria_update"),
+    path("ejerciciocategoria/delete/<int:pk>/", views.EjercicioCategoriaDelete.as_view(), name="ejerciciocategoria_delete"),
     path("tema/create/", views.tema_create, name="tema_create"),
     path("solucion/create/", views.solucion_create, name="solucion_create"),
 ]
--- project/Clase/views.py
+++ project/Clase/views.py
@@ -1,21 +1,59 @@
 from django.shortcuts import render, redirect
-
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from . import models, forms
 
 def home(request):
-    query = models.Ciclo.objects.all().prefetch_related('cursos__profesor', 'cursos__temas__ejercicios__solucion')
-    context = {'ciclos': query}
-    return render(request, 'Clase/index.html', context)
+    return render(request, 'Clase/index.html')
 
-def ejerciciocategoria_create(request):
+"""def ejerciciocategoria_list(request):
+    consulta = request.GET.get("consulta", None)
+    if consulta:
+        cursos = models.Curso.objects.filter(nombre__icontains=consulta)
+    else:
+        cursos = models.Curso.objects.all()
+    context = {"cursos": cursos}
+    return render(request, 'Clase/ejerciciocategoria_list.html', context)"""
+
+class EjercicioCategoriaCreate(CreateView):
+    model = models.EjercicioCategoria
+    form_class = forms.EjercicioCategoriaForm
+    success_url = reverse_lazy('Clase:home')
+
+class EjercicioCategoriaUpdate(UpdateView):
+    model = models.EjercicioCategoria
+    form_class = forms.EjercicioCategoriaForm
+    success_url = reverse_lazy('Clase:ejerciciocategoria_list')
+
+class EjercicioCategoriaDelete(DeleteView):
+    model = models.EjercicioCategoria
+    success_url = reverse_lazy('Clase:ejerciciocategoria_list')
+
+class EjercicioCategoriaList(ListView):
+    model = models.EjercicioCategoria
+    context_object_name = 'ejercicios'
+    template_name = 'Clase/ejerciciocategoria_list.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cursos'] = models.Curso.objects.all()
+        return context
+
+class EjercicioCategoriaDetail(DetailView):
+    model = models.EjercicioCategoria
+    context_object_name = 'ejercicio'
+    template_name = 'Clase/ejerciciocategoria_detail.html'
+
+
+"""def ejerciciocategoria_create(request):
     if request.method == 'POST':
         form = forms.EjercicioCategoriaForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('Clase:home')
-    else: #request.method == 'GET':
+    else:  # request.method == 'GET':
         form = forms.EjercicioCategoriaForm()
-    return render(request, 'Clase/ejerciciocategoria_create.html', context={'form': form})
+    return render(request, 'Clase/ejerciciocategoria_create.html', context={'form': form})"""
 
 def tema_create(request):
     if request.method == 'POST':
@@ -23,7 +61,7 @@ def tema_create(request):
         if form.is_valid():
             form.save()
             return redirect('Clase:home')
-    else: #request.method == 'GET':
+    else:  # request.method == 'GET':
         form = forms.TemaForm()
     return render(request, 'Clase/tema_create.html', context={'form': form})
 
@@ -33,6 +71,6 @@ def solucion_create(request):
         if form.is_valid():
             form.save()
             return redirect('Clase:home')
-    else: #request.method == 'GET':
+    else:  # request.method == 'GET':
         form = forms.SolucionForm()
     return render(request, 'Clase/solucion_create.html', context={'form': form})
