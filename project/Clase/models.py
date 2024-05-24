e6df97ea99d8ba7a43196b91c64944f2a8f519c5
Jean Villegas <jeanvillegaslop@gmail.com>
2024-05-23 23:54:28 -0500

    add final

--- project/Clase/models.py
+++ project/Clase/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
 class Ciclo(models.Model):
     nombre = models.CharField(max_length=100, unique=True, blank=True, null=True)
@@ -45,3 +46,10 @@ class Solucion(models.Model):
     
     def __str__(self):
         return f"Solución del ejercicio número {self.ejercicio_id.id}"
+
+class Estudiante(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name='estudiante')
+    avatar = models.ImageField(upload_to='avatares/', blank=True, null=True)
+
+    def __str__(self):
+        return self.usuario.username
--- project/core/urls.py
+++ project/core/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.CustomLoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(template_name="core/logout.html"), name="logout"),
-    path('register/', views.register, name="register")
+    path('register/', views.register, name="register"),
+    path('about/', views.about, name="about"),
 ]
 
 if settings.DEBUG:
